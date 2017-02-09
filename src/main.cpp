c81cca3b447440e42cf1f17c38f2e3d9e649925d
Ethan Steinberg <ethan.steinberg@gmail.com>
2017-02-08 20:48:36 -0600

    Add option for premultiplied alpha

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,29 @@
 int main(int argv, char **argc) {
     std::vector<std::string> files;
 
+    bool premultipy_alpha = false;
+
     for (int i = 1; i < argv; i++) {
-      files.push_back(argc[i]);
+        if (argc[i] == std::string("-premultipy")) {
+            premultipy_alpha = true;
+        } else {
+           files.push_back(argc[i]);
+        }
     }
 
     auto result = process(files);
 
     auto& image = result.first;
+
+    if (premultipy_alpha) {
+        for (unsigned int x = 0; x < image.width; x++) {
+           for (unsigned int y = 0; y < image.height; y++) {
+                for (int c = 0; c < 3; c++) {
+                    image.pixels[y * image.width * 4 + x * 4 + c] *= image.pixels[y * image.width * 4 + x * 4 + 3]/255.0;
+                }
+            }
+        }
+    }
 
     unsigned error = lodepng::encode("pixelPacker.png", image.pixels, image.width, image.height);
 
